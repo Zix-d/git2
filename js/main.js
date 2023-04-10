04546f1b88de097063f5d961f6c743ae493f2fe4
Doston <aingaa848@gmail.com>
2023-04-10 16:19:33 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let userName = "Doston";
+console.log(userName);
+userName = 'bottle';
+console.log(userName);
+
+let userAge = 5;
+userAge = 25;
+console.log(userAge)
+let price = 100;
+console.log('this item costs price' + " " + price + '$');
